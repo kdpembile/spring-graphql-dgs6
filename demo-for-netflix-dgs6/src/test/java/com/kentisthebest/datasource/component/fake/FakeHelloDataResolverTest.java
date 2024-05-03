516b5c04fb9a292a2fa934713f0f3dd852f06ce9
KD <kdpembile@securitybank.com.ph>
2024-05-03 17:21:32 +0800

    Added unit test

---
+++ demo-for-netflix-dgs6/src/test/java/com/kentisthebest/datasource/component/fake/FakeHelloDataResolverTest.java
@@ -0,0 +1,56 @@
+package com.kentisthebest.datasource.component.fake;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.netflix.graphql.dgs.DgsQueryExecutor;
+import java.util.List;
+import org.intellij.lang.annotations.Language;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeHelloDataResolverTest {
+
+  @Autowired
+  private DgsQueryExecutor dgsQueryExecutor;
+
+  @Test
+  void allHellos() {
+    @Language("GraphQL") var graphQLQuery = """
+        query AllHellos {
+            allHellos {
+                text
+                randomNumber
+            }
+        }
+        """;
+
+    List<String> texts = dgsQueryExecutor.executeAndExtractJsonPath(graphQLQuery, "data.allHellos[*].text");
+    List<Integer> randomNumbers = dgsQueryExecutor.executeAndExtractJsonPath(graphQLQuery, "data.allHellos[*].randomNumber");
+
+    assertNotNull(texts);
+    assertFalse(texts.isEmpty());
+    assertNotNull(randomNumbers);
+    assertEquals(texts.size(), randomNumbers.size());
+  }
+
+  @Test
+  void oneHello() {
+    @Language("GraphQL") var graphQLQuery = """
+        query OneHello {
+            oneHello {
+                text
+                randomNumber
+            }
+        }
+                
+        """;
+    String text = dgsQueryExecutor.executeAndExtractJsonPath(graphQLQuery, "data.oneHello.text");
+    Integer randomNumber = dgsQueryExecutor.executeAndExtractJsonPath(graphQLQuery, "data.oneHello.randomNumber");
+
+    assertFalse(StringUtils.isBlank(text));
+    assertNotNull(randomNumber);
+  }
+}
