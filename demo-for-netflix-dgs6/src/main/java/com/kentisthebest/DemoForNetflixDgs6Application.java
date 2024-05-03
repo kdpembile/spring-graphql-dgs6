1f85312a1e3f52e5d36bce90716f098bb6d01ac4
KD <kdpembile@securitybank.com.ph>
2024-05-03 12:19:12 +0800

    Initial commit

---
+++ demo-for-netflix-dgs6/src/main/java/com/kentisthebest/DemoForNetflixDgs6Application.java
@@ -0,0 +1,13 @@
+package com.kentisthebest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForNetflixDgs6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForNetflixDgs6Application.class, args);
+	}
+
+}
---
+++ demo-for-netflix-dgs6/src/test/java/com/kentisthebest/DemoForNetflixDgs6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kentisthebest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForNetflixDgs6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
